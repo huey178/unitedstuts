2db7195f9344004d52e6ec66927c4f2daf08e4e7
Tevin Sharp <sharp@Tevins-MacBook-Air-7.local>
2020-01-25 11:00:44 -0600

    Connected mongodb atlas to backend

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
