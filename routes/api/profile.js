05cfd7314a6004d27784daad1a205437862cd452
Tevin Sharp <sharp@Tevins-MacBook-Air-7.local>
2020-02-08 12:40:34 -0600

    Added github repo route and delete education route. Profile route is now complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const authen = require("../../middleware/authen");
 const Profile = require("../../models/Profile");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+const request = require("request");
+const config = require("config");
 
 //@route  api/profile/me
 //@desc   Get Current Profile
@@ -280,15 +282,62 @@ router.put(
     };
     try {
       const profile = await Profile.findOne({ user: req.user.id });
-      profile.experience.unshift(edu);
+      profile.education.unshift(edu);
 
       await profile.save();
-      res.json(body);
+      res.json(profile);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
   }
 );
+
+router.delete("/education/:edu_id", authen, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   Get api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" }
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "User was not found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
