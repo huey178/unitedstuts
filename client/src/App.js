446a88d17f53ab5caec17be934d991010354dc30
Tevin Sharp <sharp@Tevins-MacBook-Air-7.local>
2020-02-18 13:03:01 -0600

    added redux and alert actions/reducers

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,19 @@ import Navbar from "./components/layout/navbar";
 import Landing from "./components/layout/landing";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+import Alert from "./components/layout/alert";
+//react redux
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
+const App = () => (
+  <Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
@@ -20,7 +25,7 @@ function App() {
         </section>
       </Fragment>
     </Router>
-  );
-}
+  </Provider>
+);
 
 export default App;
